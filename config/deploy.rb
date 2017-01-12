e7608691c6ec42157c80872dafceb4115dd7db12
Jack Reed <phillipjreed@gmail.com>
2017-01-12 12:02:43 -0800

    update deploy info

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'datagov'
 set :repo_url, 'https://github.com/mejackreed/datagov.git'
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :user, 'geostaff'
+set :user, 'datagov'
 set :home_directory, "/opt/app/#{fetch(:user)}"
 
 set :deploy_to, "#{fetch(:home_directory)}/#{fetch(:application)}"
@@ -19,7 +19,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml config/blacklight.yml public/robots.txt}
+set :linked_files, %w{config/database.yml config/blacklight.yml public/robots.txt}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{config/settings log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/sitemaps}
--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,4 +1,4 @@
-server 'kurma-geo4libcamp-prod.stanford.edu', user: 'geostaff', roles: %w(web db app)
+server 'sul-datagov-prod.stanford.edu', user: 'datagov', roles: %w(web db app)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
